c002167a96f7b3a7cdda95825b3b8ec2f965c838
Adam Thompson <adam@serialphotog.com>
2020-12-22 14:18:07 -0500

    Fix build

--- lang/OLS_en_US.ts
+++ lang/OLS_en_US.ts
@@ -4,30 +4,37 @@
 <context>
     <name>OLS::MainWindow</name>
     <message>
+        <location filename="../src/ui/main_window.cpp" line="13"/>
         <source>Open Landscape Stacker</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/ui/main_window.cpp" line="28"/>
         <source>&amp;File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/ui/main_window.cpp" line="29"/>
         <source>&amp;Exit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/ui/main_window.cpp" line="34"/>
         <source>&amp;Help</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/ui/main_window.cpp" line="35"/>
         <source>About</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/ui/main_window.cpp" line="42"/>
         <source>About Open Landscape Stacker</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/ui/main_window.cpp" line="43"/>
         <source>A work in progress landscape stacker by Adam Thompson.</source>
         <translation type="unfinished"></translation>
     </message>
